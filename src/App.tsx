579c76c7a6fa08167e956a6c68be3ff35e94ef22
mgorbman <mgorbman@gmail.com>
2022-06-26 17:45:08 +0300

    Editor component imported into the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Editor from './components/Editor';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [state, setState] = useState("<p>Hello from CKEditor 5!</p>")
+  return (<div> 
+    <Editor
+    data={state} 
+    submit={setState}
+    />
+    {JSON.stringify(state)}
+  </div>)
 }
 
 export default App;
