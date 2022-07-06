c255e03acb68a1413177bff3164f0e2482b85988
mgorbman <mgorbman@gmail.com>
2022-07-06 14:04:19 +0300

    added routing based on component structure

--- src/App.tsx
+++ src/App.tsx
@@ -7,28 +7,9 @@ import {x} from './Api'
 import { BrowserRouter, Route, Router, Routes} from 'react-router-dom';
 
 function App() {
-const [state, setState] = useState("<p>Hello from CKEditor 5!</p>")
+
   return (
-  <div> 
-    <h1>Editor</h1>
-    {/* <Route path='test' element={<h2>test</h2>}/> */}
-
-    <Routes>
-      <Route path="/" element={<h2>test</h2>} />
-    </Routes>
-    
-    
-    
-    
-    
-  
-
-    <button onClick={x}>Get</button>
-    <Editor
-    data={state} 
-    submit={setState}
-    />
-    {JSON.stringify(state)}
+  <div className='App'> 
   </div>)
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,20 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import Homepage from './components/Homepage';
+import CV from './components/CV';
+import Blog from './components/Blog';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
+  <Routes>
+      <Route path="/" element={<Homepage/>} />
+      <Route path="CV" element={<CV/>} />
+      <Route path="Blog" element={<Blog/>} />
+    </Routes>
     <App />
   </BrowserRouter>
 
