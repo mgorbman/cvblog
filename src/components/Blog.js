4010fd5ff135178b0a8a9c1fb318160fb984d977
mgorbman <mgorbman@gmail.com>
2022-07-12 14:27:36 +0300

    Fixed html string rendering in Blog component

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,17 +2,21 @@
 import { useParams } from 'react-router-dom'
 import { getPostByURL } from '../Api'
 import { useState } from 'react'
+import parse from 'html-react-parser'
 
 export default (props) => {
     const [state, setState] = useState(null)
     const { title } = useParams()
+    //This conditional is a naive solution (assuming a single render)
     if (state == null) {
         getPostByURL(title, setState)
+        return <div>Loading data</div>
     }
     return (
         <>
             <h2>Blog</h2>
             {JSON.stringify(state)}
+            <div>{parse(state.content)}</div>
         </>
     )
 }
