a53c030a478a382a560b3a62803e9ede9f0a66ff
mgorbman <mgorbman@gmail.com>
2022-08-02 10:44:28 +0300

    Changed implementation of Api.js functions (now done with async await)

--- src/Api.js
+++ src/Api.js
@@ -1,25 +1,40 @@
-const getPostByURL = (url, callback) => {
-    fetch(`${process.env.REACT_APP_X}/posts/${url}`)
-        .then((response) => response.json())
-        .then((x) => callback(x))
+async function getPostByURL(url, callback) {
+    const response = await fetch(`${process.env.REACT_APP_X}/posts/${url}`)
+    callback(await response.json())
 }
 
-const getPostList = (callback) => {
-    fetch(`${process.env.REACT_APP_X}/blog`)
-        .then((response) => response.json())
-        .then((x) => callback(x))
+// const getPostList = (callback) => {
+//     fetch(`${process.env.REACT_APP_X}/blog`)
+//         .then((response) => response.json())
+//         .then((x) => callback(x))
+// }
+
+async function getPostList(callback) {
+    const response = await fetch(`${process.env.REACT_APP_X}/blog`)
+    callback(await response.json())
 }
 
-const pushPost = (data, callback) => {
-    fetch(`${process.env.REACT_APP_X}/pushPost`, {
+// const pushPost = (data, callback) => {
+//     fetch(`${process.env.REACT_APP_X}/pushPost`, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(data),
+//     })
+//         .then((response) => response.json())
+//         .then((x) => callback(x))
+// }
+
+async function pushPost(data, callback) {
+    const response = await fetch(`${process.env.REACT_APP_X}/pushPost`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(data),
     })
-        .then((response) => response.json())
-        .then((x) => callback(x))
+    callback(await response.json())
 }
 
 export { getPostByURL, getPostList, pushPost }
